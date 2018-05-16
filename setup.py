8f65c1070f0065c33b642030f83667ad0c3a471a
yjs <15757160531@163.com>
2018-05-16 09:31:11 +0800

    update

---
+++ setup.py
@@ -0,0 +1,30 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
+import sys, json
+"""
+打包的用的setup必须引入，
+"""
+VERSION = '0.0.3'
+setup(name='youdao.cli',
+      version=VERSION,
+      description="A youdao simple douban cli based on Python",
+      long_description='just enjoy',
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python youdao youdao.cli terminal',
+      author='xiaozhizi',
+      author_email='xiaozhizi@163.com',
+      url='',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'clint',
+        'requests',
+      ],
+      entry_points={
+        'console_scripts':[
+            'youdai = youdao.cli:main'
+        ]
+      },
+)
